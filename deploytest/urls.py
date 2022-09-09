092cef2e6c7333a7f20249d0a0ca7bd47d519a49
Shivam Chaurasia <s_chaurasia@apple.com>
2022-09-09 10:50:01 +0530

    Test

--- deploytest/urls.py
+++ deploytest/urls.py
@@ -1,4 +1,4 @@
-"""test_project1 URL Configuration
+"""deploytest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
